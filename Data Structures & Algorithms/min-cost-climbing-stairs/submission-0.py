a7a71eb0cc8e2200fa340f11b83789739fa64d03
Parth Kulkarni <91055071+ParthK7@users.noreply.github.com>
2026-05-11 22:54:06 -0700

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        def dfs(index):
+            if index >= len(cost):
+                return 0
+            return cost[index] + min(dfs(index + 1), dfs(index + 2))
+        
+        return min(dfs(0), dfs(1))
